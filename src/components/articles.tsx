ae493950cdc1814b2ca5cacff41993903c0e19ea
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-07-07 00:39:59 +0900

    SET

--- src/components/articles.tsx
+++ src/components/articles.tsx
@@ -44,7 +44,7 @@ export const Articles = ({ posts, slug, total, currentNum }: Props) => {
                     className="thum-img"
                   />
                 </div>
-                {post.updatedAt.slice(0, 10)}
+                {post.publishedAt.slice(0, 10)}
                 <br />
                 <strong>{post.title}</strong>
                 {post.content
